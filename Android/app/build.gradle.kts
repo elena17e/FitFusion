1805bb1dcdf679f440ccec4a6a3782b9d3856cfb
AndrejHre123 <ahrebakpa@student.foi.hr>
2023-11-04 21:02:48 +0100

    Napravljena registracijska forma, incijalni zaslon, dva gumba (sign up i login), Sign up vodi na formu za registraciju. Forma je povezana sa bazom podataka i napravljen je kod za unos u bazu podataka ali nije dovrsen.

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -65,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
