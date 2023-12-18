182cf4432d76d029fe0166db32eea2b8fd5f3936
elena17e <pizekelena3@gmail.com>
2023-12-18 10:49:46 +0100

    Warnings were removed from calendar_cell.xml and CalendarAdapter. In fragment_calendar.xml calendar was created with text and recyclerview. In CalendarFragment.kt calendar  from fragment_calendar.xml is shown and onClick functions for buttons were created as well as function that shows selected date. FIT-71

--- Android/app/src/main/java/hr/foi/air/fitfusion/fragments/CalendarFragment.kt
+++ Android/app/src/main/java/hr/foi/air/fitfusion/fragments/CalendarFragment.kt
@@ -1,17 +1,86 @@
 package hr.foi.air.fitfusion.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.air.fitfusion.R
+import hr.foi.air.fitfusion.adapters.CalendarAdapter
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
-class CalendarFragment : Fragment() {
+
+class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
+    private lateinit var monthYearText: TextView
+    private lateinit var calendarRecyclerView: RecyclerView
+    private lateinit var selectedDate: LocalDate
+    private lateinit var btnBackCl : Button
+    private lateinit var btnForwardCl : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        monthYearText = view.findViewById(R.id.monthYearTV)
+        calendarRecyclerView = view.findViewById(R.id.calendarRecyclerView)
+        selectedDate = LocalDate.now()
+        setMonthView()
+        btnBackCl = view.findViewById(R.id.btnBackCl)
+        btnForwardCl = view.findViewById(R.id.btnForwardCl)
+        btnBackCl.setOnClickListener {
+            selectedDate = selectedDate.minusMonths(1)
+            setMonthView()
+        }
+        btnForwardCl.setOnClickListener {
+            selectedDate = selectedDate.plusMonths(1)
+            setMonthView()
+        }
+    }
+    private fun setMonthView() {
+        monthYearText.text = monthYearFromDate(selectedDate)
+        val daysInMonth = daysInMonthArray(selectedDate)
+        val calendarAdapter = CalendarAdapter(daysInMonth, this)
+        val layoutManager: RecyclerView.LayoutManager =
+            GridLayoutManager(context, 7)
+        calendarRecyclerView.layoutManager = layoutManager
+        calendarRecyclerView.adapter = calendarAdapter
+    }
+
+    private fun daysInMonthArray(date: LocalDate): ArrayList<String> {
+        val daysInMonthArray = ArrayList<String>()
+        val yearMonth = YearMonth.from(date)
+        val daysInMonth = yearMonth.lengthOfMonth()
+        val firstOfMonth = selectedDate.withDayOfMonth(1)
+        val dayOfWeek = firstOfMonth.dayOfWeek.value
+        for (i in 1..42) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add("")
+            } else {
+                daysInMonthArray.add((i - dayOfWeek).toString())
+            }
+        }
+        return daysInMonthArray
+    }
+
+    private fun monthYearFromDate(date: LocalDate): String? {
+        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
+        return date.format(formatter)
+    }
+
+    override fun onItemClick(position: Int, dayText: String) {
+        if (dayText.isNotEmpty()) {
+            val message = "Selected Date $dayText ${monthYearFromDate(selectedDate)}"
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
