a75d1258b6e8fa5504c496f106bc2010a3cfb0d2
elena17e <pizekelena3@gmail.com>
2023-12-18 09:51:12 +0100

    Previous calendar was removed. calendar_cell.xml was created and it will be used for cells in calendar. CalendarAdapter and CalendarViewHolder were created to show calendar cells. FIT-71

---
+++ Android/app/src/main/java/hr/foi/air/fitfusion/views/CalendarViewHolder.kt
@@ -0,0 +1,18 @@
+package hr.foi.air.fitfusion.views
+import hr.foi.air.fitfusion.R
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hr.foi.air.fitfusion.adapters.CalendarAdapter
+
+class CalendarViewHolder (itemView: View, private val onItemListener: CalendarAdapter.OnItemListener) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    val dayOfMonth: TextView = itemView.findViewById(R.id.cellDayText)
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        onItemListener.onItemClick(adapterPosition, dayOfMonth.text.toString())
+    }
+}
