6c93323dcf12dbef868ebccd17cafabc28c6f8b8
ejankovic <ejankovic@student.foi.hr>
2024-01-02 12:28:52 +0100

    Added the new activity and xml file for displaying weekly calendar view. The implementation is not complete. For now, only the UI part is done. Jira task: FIT-28

---
+++ Android/app/src/main/java/hr/foi/air/fitfusion/WeeklyViewCalendarActivity.kt
@@ -0,0 +1,11 @@
+package hr.foi.air.fitfusion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeeklyViewCalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly_view_calendar)
+    }
+}
