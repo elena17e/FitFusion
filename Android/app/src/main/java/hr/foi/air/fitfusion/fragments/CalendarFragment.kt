32507c04c1b80c4e86758ac24abf076dc152eb3e
elena17e <pizekelena3@gmail.com>
2024-01-15 11:07:58 +0100

    Button weekly was removed from our calendar view and instead WeekViewActivity.kt opens when clicking on specific day. Some warnings were also fixed. FIT-27

--- Android/app/src/main/java/hr/foi/air/fitfusion/fragments/CalendarFragment.kt
+++ Android/app/src/main/java/hr/foi/air/fitfusion/fragments/CalendarFragment.kt
@@ -23,41 +23,34 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
     private var calendarRecyclerView: RecyclerView? = null
     private lateinit var btnBackCl : Button
     private lateinit var btnForwardCl : Button
-    private lateinit var btnWeeklyCl : Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Log.d("CalendarFragment", "Initializing selectedDate")
-        //CalendarUtils.selectedDate = LocalDate.now()
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         monthYearText = view.findViewById(R.id.monthYearTV)
         calendarRecyclerView = view.findViewById(R.id.calendarRecyclerView)
         if (CalendarUtils.selectedDate == null) {CalendarUtils.selectedDate = LocalDate.now()}
-        //CalendarUtils.selectedDate = LocalDate.now()
         setMonthView()
         btnBackCl = view.findViewById(R.id.btnBackCl)
         btnForwardCl = view.findViewById(R.id.btnForwardCl)
-        btnWeeklyCl = view.findViewById(R.id.btnWeekly)
         btnBackCl.setOnClickListener {
-            previousMonthAction(btnBackCl)
+            previousMonthAction()
         }
         btnForwardCl.setOnClickListener {
-            nextMonthAction(btnForwardCl)
-        }
-        btnWeeklyCl.setOnClickListener {
-            weeklyAction(btnWeeklyCl)
+            nextMonthAction()
         }
     }
 
-    fun previousMonthAction(view: View?) {
+    private fun previousMonthAction() {
         CalendarUtils.selectedDate = CalendarUtils.selectedDate!!.minusMonths(1)
         setMonthView()
     }
 
-    fun nextMonthAction(view: View?) {
+    private fun nextMonthAction() {
         CalendarUtils.selectedDate = CalendarUtils.selectedDate!!.plusMonths(1)
         setMonthView()
     }
@@ -74,46 +67,16 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
     override fun onResume() {
         super.onResume()
         if (CalendarUtils.selectedDate != null) {
-            setMonthView() // This will refresh the month view with the updated date
+            setMonthView()
         }
     }
-
-    /*private fun daysInMonthArray(date: LocalDate): ArrayList<String> {
-        val daysInMonthArray = ArrayList<String>()
-        val yearMonth = YearMonth.from(date)
-        val daysInMonth = yearMonth.lengthOfMonth()
-        val firstOfMonth = selectedDate.withDayOfMonth(1)
-        val dayOfWeek = firstOfMonth.dayOfWeek.value
-        for (i in 1..42) {
-            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
-                daysInMonthArray.add("")
-            } else {
-                daysInMonthArray.add((i - dayOfWeek).toString())
-            }
-        }
-        return daysInMonthArray
-    }*/
-
-    /*private fun monthYearFromDate(date: LocalDate): String? {
-        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-        return date.format(formatter)
-    }
-
-    override fun onItemClick(position: Int, dayText: String) {
-        if (dayText.isNotEmpty()) {
-            val message = "Selected Date $dayText ${monthYearFromDate(selectedDate)}"
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        }
-    }*/
 
     override fun onItemClick(position: Int, date: LocalDate?) {
         if (date != null) {
             CalendarUtils.selectedDate = date
             setMonthView()
+            startActivity(Intent(requireContext(), WeekViewActivity::class.java))
         }
     }
 
-    fun weeklyAction(view: View) {
-        startActivity(Intent(requireContext(), WeekViewActivity::class.java))
-    }
 }
