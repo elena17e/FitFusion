3814716013b266cc99a0aaae318e4b4672603b77
DESKTOP-DCA8JV9\Dora <dora.sandrovcan1@gmail.com>
2024-01-18 01:25:28 +0100

    Users can now see on homepage trainings they applied to. Button on homepage next to "Booked classes" leads to calendar.
    FIT-74

--- Android/app/src/main/java/hr/foi/air/fitfusion/adapters/TrainingHomepageAdapter.kt
+++ Android/app/src/main/java/hr/foi/air/fitfusion/adapters/TrainingHomepageAdapter.kt
@@ -3,13 +3,12 @@ package hr.foi.air.fitfusion.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.air.fitfusion.R
 import hr.foi.air.fitfusion.entities.Training
 
-class TrainingHomepageAdapter(private val trainingsList: ArrayList<Training>, private val onCalendarClick:()-> Unit) : RecyclerView.Adapter<TrainingHomepageAdapter.TrainingViewHolder>() {
+class TrainingHomepageAdapter(private val trainingsList: ArrayList<Training>) : RecyclerView.Adapter<TrainingHomepageAdapter.TrainingViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrainingViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_booked_class, parent, false)
@@ -19,21 +18,18 @@ class TrainingHomepageAdapter(private val trainingsList: ArrayList<Training>, pr
     override fun getItemCount(): Int = trainingsList.size
 
     override fun onBindViewHolder(holder: TrainingViewHolder, position: Int) {
-        holder.bind(trainingsList[position], onCalendarClick)
+        holder.bind(trainingsList[position])
     }
 
     class TrainingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val type: TextView = itemView.findViewById(R.id.type)
         private val date: TextView = itemView.findViewById(R.id.date)
         private val time: TextView = itemView.findViewById(R.id.time)
-        //private val imageButton: ImageButton = itemView.findViewById(R.id.addTraining)
 
-        fun bind(training: Training, onCalendarClick: () -> Unit) {
+        fun bind(training: Training) {
             type.text = training.type
             date.text = training.date
             time.text = training.time
-
-            //imageButton.setOnClickListener {                onCalendarClick()           }
         }
     }
 }
